c7462ce5ca3350084655d4f4be0afa115e9cf929
Saravanan <psaravanan11@gmail.com>
2016-04-23 00:52:45 +0530

    ui improved and aligned

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@ resources :appointment
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+   get 'appointments' => 'appointment#appointments'
    root 'appointment#new'
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
